3d984dd7c467fca449dcb76a4fe1801f8021979b
Михаил Кучук <kuchuk.m.s@gmail.com>
2020-12-02 10:37:43 +0300

    Провел рефакторинг тестов с использованием Setup Test

--- TDDKata/test.cpp
+++ TDDKata/test.cpp
@@ -1,235 +1,231 @@
 #include "pch.h"
 #include "Calculator.h"
+#include "TestFixture.h"
 #include <string>
 
 
-	TEST(TDDKata, EmptyArgsTest) {
+	TEST_F(TestFixture, EmptyArgsTest) {
 		std::string arg = "";
 		auto expected = 0;
-		Calculator calc;
 
-		auto result = calc.Add(arg);
+		auto result = calc->Add(arg);
 		ASSERT_EQ(expected, result);
 	}
 
 
-	TEST(TDDKata, OneArgumentTest) {
+	TEST_F(TestFixture, OneArgumentTest) {
 		std::string arg = "1";
 		auto expected = 1;
-		Calculator calc;
 
-		auto result = calc.Add(arg);
+		auto result = calc->Add(arg);
 		ASSERT_EQ(expected, result);
 	}
 
 
-	TEST(TDDKata, OneNegativeArgumentTest) {
+	TEST_F(TestFixture, OneNegativeArgumentTest) {
 		std::string arg = "-999";
 		auto expected = -1;
-		Calculator calc;
 
-		auto result = calc.Add(arg);
+		auto result = calc->Add(arg);
 		ASSERT_EQ(expected, result);
 	}
 
 
-	TEST(TDDKata, SummWithZeroTest) {
+	TEST_F(TestFixture, SummWithZeroTest) {
 		std::string arg = "0,2";
 		auto expected = 2;
-		Calculator calc;
 
-		auto result = calc.Add(arg);
+		auto result = calc->Add(arg);
 		ASSERT_EQ(expected, result);
 	}
 
 
-	TEST(TDDKata, SummTest) {
+	TEST_F(TestFixture, SummTest) {
 		std::string arg = "1,20";
 		auto expected = 21;
-		Calculator calc;
 
-		auto result = calc.Add(arg);
+		auto result = calc->Add(arg);
 		ASSERT_EQ(expected, result);
 	}
 
 
-	TEST(TDDKata, FirstNegativeArgsTest) {
+	TEST_F(TestFixture, FirstNegativeArgsTest) {
 		std::string arg = "-1,1";
 		auto expected = -1;
-		Calculator calc;
 
-		auto result = calc.Add(arg);
+		auto result = calc->Add(arg);
 		ASSERT_EQ(expected, result);
 	}
 
 
-	TEST(TDDKata, SecondNegativeArgsTest) {
+	TEST_F(TestFixture, SecondNegativeArgsTest) {
 		std::string arg = "1,-1";
 		auto expected = -1;
-		Calculator calc;
 
-		auto result = calc.Add(arg);
+		auto result = calc->Add(arg);
 		ASSERT_EQ(expected, result);
 	}
 
 
-	TEST(TDDKata, AllNegativeArgsTest) {
+	TEST_F(TestFixture, AllNegativeArgsTest) {
 		std::string arg = "-2,-1";
 		auto expected = -1;
-		Calculator calc;
 
-		auto result = calc.Add(arg);
+		auto result = calc->Add(arg);
 		ASSERT_EQ(expected, result);
 	}
 
 
-	TEST(TDDKata, FirstInvalidArgsTest) {
+	TEST_F(TestFixture, FirstInvalidArgsTest) {
 		std::string arg = "a,3";
 		auto expected = -2;
-		Calculator calc;
 
-		auto result = calc.Add(arg);
+		auto result = calc->Add(arg);
 		ASSERT_EQ(expected, result);
 	}
 
 
-	TEST(TDDKata, SecondInvalidArgsTest) {
+	TEST_F(TestFixture, SecondInvalidArgsTest) {
 		std::string arg = "3,a";
 		auto expected = -2;
-		Calculator calc;
 
-		auto result = calc.Add(arg);
+		auto result = calc->Add(arg);
 		ASSERT_EQ(expected, result);
 	}
 
 
-	TEST(TDDKata, SymbolArgsTest) {
+	TEST_F(TestFixture, SymbolArgsTest) {
 		std::string arg = "a";
 		auto expected = -2;
-		Calculator calc;
 
-		auto result = calc.Add(arg);
+		auto result = calc->Add(arg);
 		ASSERT_EQ(expected, result);
 	}
 
 
-	TEST(TDDKata, AllSymbolArgsTest) {
+	TEST_F(TestFixture, AllSymbolArgsTest) {
 		std::string arg = "a,b";
 		auto expected = -2;
-		Calculator calc;
 
-		auto result = calc.Add(arg);
+		auto result = calc->Add(arg);
 		ASSERT_EQ(expected, result);
 	}
 
 
-	TEST(TDDKata, SeparatorTest) {
+	TEST_F(TestFixture, SeparatorTest) {
 		std::string arg = "0.1";
 		auto expected = -3;
-		Calculator calc;
 
-		auto result = calc.Add(arg);
+		auto result = calc->Add(arg);
 		ASSERT_EQ(expected, result);
 	}
 
 
-	TEST(TDDKata, VariableArgsTest) {
+	TEST_F(TestFixture, VariableArgsTest) {
 		std::string arg = "0,1,2";
 		auto expected = 3;
-		Calculator calc;
 
-		auto result = calc.Add(arg);
+		auto result = calc->Add(arg);
 		ASSERT_EQ(expected, result);
 	}
 
 
-	TEST(TDDKata, Variable2ArgsTest) {
+	TEST_F(TestFixture, Variable2ArgsTest) {
 		std::string arg = "0,1,2,10,100";
 		auto expected = 113;
-		Calculator calc;
 
-		auto result = calc.Add(arg);
+		auto result = calc->Add(arg);
 		ASSERT_EQ(expected, result);
 	}
 
 
-	TEST(TDDKata, NexLineTwoArgsTest) {
+	TEST_F(TestFixture, NexLineTwoArgsTest) {
 		std::string arg = "1\n2";
 		auto expected = 3;
-		Calculator calc;
 
-		auto result = calc.Add(arg);
+		auto result = calc->Add(arg);
 		ASSERT_EQ(expected, result);
 	}
 
 
-	TEST(TDDKata, NexLineArgsTest) {
+	TEST_F(TestFixture, NexLineArgsTest) {
 		std::string arg = "1\n2,3";
 		auto expected = 6;
-		Calculator calc;
 
-		auto result = calc.Add(arg);
+		auto result = calc->Add(arg);
 		ASSERT_EQ(expected, result);
 	}
 
 
-	TEST(TDDKata, IncorrectNexLineArgsTest) {
+	TEST_F(TestFixture, IncorrectNexLineArgsTest) {
 		std::string arg = "1,\n";
 		auto expected = -4;
-		Calculator calc;
 
-		auto result = calc.Add(arg);
+		auto result = calc->Add(arg);
 		ASSERT_EQ(expected, result);
 	}
 
 
-	TEST(TDDKata, Token0Test) {
+	TEST_F(TestFixture, Token0Test) {
 		std::string arg = "//;\n2;3";
 		auto expected = 5;
-		Calculator calc;
 
-		auto result = calc.Add(arg);
+		auto result = calc->Add(arg);
 		ASSERT_EQ(expected, result);
 	}
 
 
-	TEST(TDDKata, Token1Test) {
+	TEST_F(TestFixture, Token1Test) {
 		std::string arg = "//$\n2$3";
 		auto expected = 5;
-		Calculator calc;
 
-		auto result = calc.Add(arg);
+		auto result = calc->Add(arg);
 		ASSERT_EQ(expected, result);
 	}
 
 
-	TEST(TDDKata, Token2Test) {
+	TEST_F(TestFixture, Token2Test) {
 		std::string arg = "//;\n2,3,5";
 		auto expected = 10;
-		Calculator calc;
 
-		auto result = calc.Add(arg);
+		auto result = calc->Add(arg);
 		ASSERT_EQ(expected, result);
 	}
 
 
-	TEST(TDDKata, Token3Test) {
+	TEST_F(TestFixture, Token3Test) {
 		std::string arg = "//;\n2\n3\n5";
 		auto expected = 10;
-		Calculator calc;
 
-		auto result = calc.Add(arg);
+		auto result = calc->Add(arg);
 		ASSERT_EQ(expected, result);
 	}
 
 
-	TEST(TDDKata, Token4Test) {
+	TEST_F(TestFixture, SeveralDelimitersTest) {
 		std::string arg = "//***\n1***2***3";
 		auto expected = 6;
-		Calculator calc;
 
-		auto result = calc.Add(arg);
+		auto result = calc->Add(arg);
+		ASSERT_EQ(expected, result);
+	}
+
+
+	TEST_F(TestFixture, SeveralDifferentDelimitersTest) {
+		std::string arg = "//@#$\n1@#$2@#$3";
+		auto expected = 6;
+
+		auto result = calc->Add(arg);
 		ASSERT_EQ(expected, result);
 	}
 
 	
+	TEST_F(TestFixture, Sample) {
+		/* Arrange */
+		std::string arg = "2,3,4,5";
+		auto expected = 14;
+		/* Act */
+		auto actual = calc->Add(arg);
+		/* Assert */
+		ASSERT_EQ(expected, actual);
+	}
